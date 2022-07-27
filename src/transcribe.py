4b5df36aac18cc1b9f542e4f979f30999fc61908
Johannes Neuhaus <johannes.neuhaus@rwth-aachen.de>
2022-07-27 23:40:32 +0200

    Determine dominant channel automatically
    This effectively implements multi-channel support which is useful for
    stereo microphones where each speaker talks into one microphone.

--- src/transcribe.py
+++ src/transcribe.py
@@ -26,6 +26,7 @@ import recasepunc
 from vosk import Model, KaldiRecognizer, SpkModel
 import numpy as np
 from sklearn.cluster import KMeans
+import librosa
 
 # must be in __main__
 from recasepunc import WordpieceTokenizer
@@ -42,12 +43,14 @@ parser.add_argument('--speakers', type=int, default=2,
                     help='Number of speakers during the interview')
 parser.add_argument('--speech-enhancement-on', action='store_true',
                     help="Use speechbrain's sppech enhancement. It usually does NOT enhance ASR accuracy")
-parser.add_argument('--ffmpeg-strategy', type=str, choices=['MERGE', 'LEFT', 'RIGHT'], default='MERGE',
+parser.add_argument('--ffmpeg-strategy', type=str, choices=['MERGE', 'LEFT', 'RIGHT', 'AUTO'], default='AUTO',
                     help="Strategy to use for creating a mono file using ffmpeg. If you use stereo microphones, you can select from 'MERGE', 'LEFT' and 'RIGHT'. Using the appropriate channel can improve accuracy.")
 parser.add_argument('--ffmpeg-noise-reduction', action='store_true', default=True,
                     help="Apply ffmpeg's NR filter.")
 parser.add_argument('--ffmpeg-compress', action='store_true', default=True,
                     help="Apply ffmpeg's compand filter for compression.")
+parser.add_argument('--no-ffmpeg-compress', dest='ffmpeg_compress', action='store_false')
+parser.add_argument('--no-ffmpeg-noise-reduction', dest='ffmpeg_noise_reduction', action='store_false')
 parser.add_argument('--log-level', type=str, default="INFO",
                     help='Set the loglevel')
 parser.add_argument('--threads', type=int, default=multiprocessing.cpu_count(),
@@ -102,6 +105,7 @@ def transcribe_audio(audio_path):
     wf = wave.open(audio_path, "rb")
 
     logging.info("Diarizing speakers into segments...")
+
     segments = diarize_speakers(audio_path, args.speakers)
     segment_texts = []
     with tempfile.TemporaryDirectory() as tmp:
@@ -109,6 +113,7 @@ def transcribe_audio(audio_path):
         for i, segment in enumerate(segments):
             logging.info(f"Preprocessing segment {i + 1}/{len(segments)}")
             filename = f'{tmp}/{i}.wav'
+            segment['path'] = filename
             start_frame = int(segment['start'] * wf.getframerate())
             end_frame = int(segment['end'] * wf.getframerate())
             length = end_frame - start_frame
@@ -136,39 +141,31 @@ def transcribe_audio(audio_path):
         for i, segment in enumerate(segments):
             rec = KaldiRecognizer(model, 16000)
             # rec.SetWords(True)
-            logging.info(f"ASR segment {i + 1}/{len(segments)}")
-            ffmpeg_options = ['ffmpeg', '-i', f'{tmp}/{i}.wav', '-ar', '16000', '-acodec', 'pcm_s16le', '-f', 'wav']
-            filter_options = []
-            if args.ffmpeg_strategy == 'MERGE':
-                ffmpeg_options.extend(['-ac', '1'])
-            elif args.ffmpeg_strategy == 'LEFT':
-                filter_options.append("pan=mono|c0=FL")
+            logging.info(f"Processing segment {segment}")
+
+            channel_to_use = args.ffmpeg_strategy
+
+            if args.ffmpeg_strategy == 'LEFT':
+                channel_to_use = 0
             elif args.ffmpeg_strategy == 'RIGHT':
-                filter_options.append("pan=mono|c0=FR")
+                channel_to_use = 1
+            elif args.ffmpeg_strategy == 'AUTO':
+                channel_to_use = determine_dominant_channel(segment['path'])
+                logging.info(f"Determined channel {channel_to_use}")
             else:
                 assert(False)
 
-            if args.ffmpeg_compress:
-                filter_options.append("compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2")
+            if channel_to_use is None:
+                channel_to_use = 'MERGE'
 
-            if args.ffmpeg_noise_reduction:
-                filter_options.append("afftdn=nr=10:nf=-40")
-
-            if len(filter_options) > 0:
-                filter_string = ','.join(filter_options)
-                ffmpeg_options.extend(['-af', filter_string])
-
-            ffmpeg_options.append('-')
-
-            # write to pipe
-            logging.debug(f"Executing ffmpeg command {' '.join(ffmpeg_options)}")
-
-            process = subprocess.Popen(ffmpeg_options, stdout=subprocess.PIPE)
+            pipe = prepare_through_ffmpeg(segment['path'], args.ffmpeg_compress, args.ffmpeg_noise_reduction, channel_to_use)
 
             recognized = ''
 
+            logging.info(f"ASR segment {i + 1}/{len(segments)}")
+
             while True:
-                data = process.stdout.read(4000)
+                data = pipe.stdout.read(4000)
                 if len(data) == 0:
                     break
                 if rec.AcceptWaveform(data):
@@ -223,6 +220,69 @@ def transcribe_audio(audio_path):
     logging.info(f"Writing result to {out_path}")
     with open(out_path, 'w') as f:
         f.write(srt.compose(srt_entries))
+
+
+
+def determine_dominant_channel(file_path):
+    ''' determine the domainant channel from a wave file. Returns the channel id (int) or None. It only
+        returns None if it is no stereo recording.
+    '''
+
+    with wave.open(file_path, 'rb') as wf:
+        if wf.getnchannels() < 2:
+            return None
+
+        # use librosa to compute rms and pick channel with strongest signal
+        strongest_rms = -1
+        strongest_channel = 0
+        sound, sample_rate = librosa.load(file_path, mono=False)
+        for i in range(0, wf.getnchannels()):
+            S, phase_left = librosa.magphase(librosa.stft(sound[i]))
+            rms = librosa.feature.rms(S=S)
+
+            current_mean_rms = np.mean(rms)
+            if current_mean_rms > strongest_rms:
+                strongest_rms = current_mean_rms
+                strongest_channel = i
+
+            logging.debug(f"{i}: rms = {current_mean_rms}")
+
+        return strongest_channel
+
+
+def prepare_through_ffmpeg(input_path, enable_compression, enable_noise_reduction, channel_to_use):
+    ffmpeg_options = ['ffmpeg']
+    if args.log_level.upper() != "DEBUG":
+        # make sure to still show errors! Otherwise we won't notice bad ffmpeg commands.
+        ffmpeg_options.extend(['-loglevel', 'error'])
+
+    ffmpeg_options.extend(['-i', input_path, '-ar', '16000', '-acodec', 'pcm_s16le', '-f', 'wav'])
+    filter_options = []
+    if channel_to_use == 'MERGE':
+        ffmpeg_options.extend(['-ac', '1'])
+    elif type(channel_to_use) == int:
+        filter_options.append(f"pan=mono|c0=c{channel_to_use}")
+    else:
+        assert(False)
+
+    if enable_compression:
+        filter_options.append("compand=.3|.3:1|1:-90/-60|-60/-40|-40/-30|-20/-20:6:0:-90:0.2")
+
+    if enable_noise_reduction:
+        filter_options.append("afftdn=nr=10:nf=-40")
+
+    if len(filter_options) > 0:
+        filter_string = ','.join(filter_options)
+        ffmpeg_options.extend(['-af', filter_string])
+
+    ffmpeg_options.append('-')
+
+    logging.debug(f"Executing ffmpeg command {' '.join(ffmpeg_options)}")
+
+    # write to pipe
+    process = subprocess.Popen(ffmpeg_options, stdout=subprocess.PIPE)
+
+    return process
 
 
 def main():
